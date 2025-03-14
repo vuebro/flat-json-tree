9191ee401dbca2fdaf6995ac2bd61adaf4701a94
Jerry Bruwes <jbruwes@icloud.com>
2025-03-14 15:16:36 +0200

    modified:   index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { toReactive } from "@vueuse/core";
 import { v4 } from "uuid";
 import { computed, isReactive, reactive } from "vue";
 
-export default function (
+export default (
   tree: Record<string, unknown>[],
   {
     branch: keyBranch = "branch",
@@ -14,9 +14,9 @@ export default function (
     prev: keyPrev = "prev",
     siblings: keySiblings = "siblings",
   } = {},
-) {
-  const configurable = true,
-    properties = {
+) => {
+  const configurable: PropertyDescriptor["configurable"] = true,
+    properties: PropertyDescriptorMap = {
       [keyBranch]: {
         get(this: Record<string, unknown>) {
           const ret = [this];
@@ -49,162 +49,151 @@ export default function (
     },
     value = isReactive(tree) ? tree : reactive(tree);
 
-  const leaves = computed(getLeaves);
+  const getLeaves = (
+      siblings: { configurable?: boolean; value: Record<string, unknown>[] },
+      parent = {},
+    ) =>
+      siblings.value.flatMap((value): Record<string, unknown>[] => {
+        Object.defineProperties(value, {
+          ...properties,
+          [keyParent]: parent,
+          [keySiblings]: siblings,
+        });
+        return [
+          value,
+          ...getLeaves(
+            {
+              configurable,
+              value: (value[keyChildren] ?? []) as Record<string, unknown>[],
+            },
+            { configurable, value },
+          ),
+        ];
+      }),
+    leaves = computed(() => getLeaves({ value }));
 
   const arrLeaves = toReactive(leaves),
-    objLeaves = toReactive(computed(getObjLeaves));
-
-  function getLeaves() {
-    return getSiblingLeaves({ value });
-  }
-
-  function getSiblingLeaves(
-    siblings: { configurable?: boolean; value: Record<string, unknown>[] },
-    parent = {},
-  ) {
-    function defineProperties(
-      value: Record<string, unknown>,
-    ): Record<string, unknown>[] {
-      Object.defineProperties(value, {
-        ...properties,
-        [keyParent]: parent,
-        [keySiblings]: siblings,
-      });
-      return [
-        value,
-        ...getSiblingLeaves(
-          {
-            configurable,
-            value: (value[keyChildren] ?? []) as Record<string, unknown>[],
-          },
-          { configurable, value },
+    objLeaves = toReactive(
+      computed(() =>
+        Object.fromEntries(
+          leaves.value.map((leaf) => [leaf[keyId] as string, leaf]),
         ),
-      ];
-    }
-    return siblings.value.flatMap(defineProperties);
-  }
+      ),
+    );
 
-  function getObjLeaves() {
-    return Object.fromEntries(leaves.value.map(getLeafEntry));
-  }
-
-  function getLeafEntry(
-    leaf: Record<string, unknown>,
-  ): [string, Record<string, unknown>] {
-    return [leaf[keyId] as string, leaf];
-  }
-
-  function add(pId: string) {
-    const the = objLeaves[pId];
-    if (the) {
-      const children = the[keyChildren] as
-          | Record<string, unknown>[]
-          | undefined,
-        index = the[keyIndex] as number,
-        siblings = the[keySiblings] as Record<string, unknown>[];
-      const id = v4();
-      switch (true) {
-        case !!the[keyParent]:
-          siblings.splice(index + 1, 0, { [keyId]: id });
-          break;
-        case !!children:
-          children.unshift({ [keyId]: id });
-          break;
-        default:
-          siblings.splice(index + 1, 0, { [keyId]: id });
-          break;
-      }
-      return id;
-    }
-    return undefined;
-  }
-
-  function down(pId: string) {
-    const the = objLeaves[pId];
-    if (the) {
-      const index = the[keyIndex] as number,
-        nextIndex = index + 1,
-        siblings = the[keySiblings] as Record<string, unknown>[];
-      if (index < siblings.length - 1 && siblings[index] && siblings[nextIndex])
-        [siblings[index], siblings[nextIndex]] = [
-          siblings[nextIndex],
-          siblings[index],
-        ];
-    }
-  }
-
-  function left(pId: string) {
-    const the = objLeaves[pId];
-    if (the) {
-      const parent = the[keyParent] as Record<string, unknown> | undefined;
-      if (parent?.[keyParent]) {
-        const children = (parent[keyChildren] ?? []) as Record<
-            string,
-            unknown
-          >[],
-          siblings = parent[keySiblings] as Record<string, unknown>[];
-        siblings.splice(
-          (parent[keyIndex] as number) + 1,
-          0,
-          ...children.splice(the[keyIndex] as number, 1),
-        );
-        return parent[keyId] as string;
-      }
-    }
-    return undefined;
-  }
-
-  function remove(pId: string) {
-    const the = objLeaves[pId];
-    if (the) {
-      const parent = the[keyParent] as Record<string, unknown> | undefined;
-      if (parent) {
-        const [root] = leaves.value,
-          next = the[keyNext] as Record<string, unknown> | undefined,
-          prev = the[keyPrev] as Record<string, unknown> | undefined,
-          id = (next?.[keyId] ??
-            prev?.[keyId] ??
-            parent[keyId] ??
-            root?.[keyId]) as string,
+  const add = (pId: string) => {
+      const the = objLeaves[pId];
+      if (the) {
+        const children = the[keyChildren] as
+            | Record<string, unknown>[]
+            | undefined,
+          index = the[keyIndex] as number,
           siblings = the[keySiblings] as Record<string, unknown>[];
-        siblings.splice(the[keyIndex] as number, 1);
+        const id = v4();
+        switch (true) {
+          case !!the[keyParent]:
+            siblings.splice(index + 1, 0, { [keyId]: id });
+            break;
+          case !!children:
+            children.unshift({ [keyId]: id });
+            break;
+          default:
+            siblings.splice(index + 1, 0, { [keyId]: id });
+            break;
+        }
         return id;
       }
-    }
-    return undefined;
-  }
-
-  function right(pId: string) {
-    const the = objLeaves[pId];
-    if (the) {
-      const prev = the[keyPrev] as Record<string, unknown> | undefined;
-      if (prev) {
-        const children = (prev[keyChildren] ?? []) as Record<string, unknown>[],
-          id = prev[keyId] as string,
+      return undefined;
+    },
+    down = (pId: string) => {
+      const the = objLeaves[pId];
+      if (the) {
+        const index = the[keyIndex] as number,
+          nextIndex = index + 1,
           siblings = the[keySiblings] as Record<string, unknown>[];
-        prev[keyChildren] = [
-          ...children,
-          ...siblings.splice(the[keyIndex] as number, 1),
-        ];
-        return id;
+        if (
+          index < siblings.length - 1 &&
+          siblings[index] &&
+          siblings[nextIndex]
+        )
+          [siblings[index], siblings[nextIndex]] = [
+            siblings[nextIndex],
+            siblings[index],
+          ];
       }
-    }
-    return undefined;
-  }
-
-  function up(pId: string) {
-    const the = objLeaves[pId];
-    if (the) {
-      const index = the[keyIndex] as number,
-        prevIndex = index - 1,
-        siblings = the[keySiblings] as Record<string, unknown>[];
-      if (index && siblings[index] && siblings[prevIndex])
-        [siblings[prevIndex], siblings[index]] = [
-          siblings[index],
-          siblings[prevIndex],
-        ];
-    }
-  }
+    },
+    left = (pId: string) => {
+      const the = objLeaves[pId];
+      if (the) {
+        const parent = the[keyParent] as Record<string, unknown> | undefined;
+        if (parent?.[keyParent]) {
+          const children = (parent[keyChildren] ?? []) as Record<
+              string,
+              unknown
+            >[],
+            siblings = parent[keySiblings] as Record<string, unknown>[];
+          siblings.splice(
+            (parent[keyIndex] as number) + 1,
+            0,
+            ...children.splice(the[keyIndex] as number, 1),
+          );
+          return parent[keyId] as string;
+        }
+      }
+      return undefined;
+    },
+    remove = (pId: string) => {
+      const the = objLeaves[pId];
+      if (the) {
+        const parent = the[keyParent] as Record<string, unknown> | undefined;
+        if (parent) {
+          const [root] = leaves.value,
+            next = the[keyNext] as Record<string, unknown> | undefined,
+            prev = the[keyPrev] as Record<string, unknown> | undefined,
+            id = (next?.[keyId] ??
+              prev?.[keyId] ??
+              parent[keyId] ??
+              root?.[keyId]) as string,
+            siblings = the[keySiblings] as Record<string, unknown>[];
+          siblings.splice(the[keyIndex] as number, 1);
+          return id;
+        }
+      }
+      return undefined;
+    },
+    right = (pId: string) => {
+      const the = objLeaves[pId];
+      if (the) {
+        const prev = the[keyPrev] as Record<string, unknown> | undefined;
+        if (prev) {
+          const children = (prev[keyChildren] ?? []) as Record<
+              string,
+              unknown
+            >[],
+            id = prev[keyId] as string,
+            siblings = the[keySiblings] as Record<string, unknown>[];
+          prev[keyChildren] = [
+            ...children,
+            ...siblings.splice(the[keyIndex] as number, 1),
+          ];
+          return id;
+        }
+      }
+      return undefined;
+    },
+    up = (pId: string) => {
+      const the = objLeaves[pId];
+      if (the) {
+        const index = the[keyIndex] as number,
+          prevIndex = index - 1,
+          siblings = the[keySiblings] as Record<string, unknown>[];
+        if (index && siblings[index] && siblings[prevIndex])
+          [siblings[prevIndex], siblings[index]] = [
+            siblings[index],
+            siblings[prevIndex],
+          ];
+      }
+    };
 
   return { add, arrLeaves, down, leaves, left, objLeaves, remove, right, up };
-}
+};
